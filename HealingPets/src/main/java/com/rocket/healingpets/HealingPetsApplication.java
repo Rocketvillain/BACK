90561c8e680280ffa104c0ae61264df4b9c2f16b
JDH <yjkl0516@gmail.com>
2024-09-11 09:45:35 +0900

    [Fix] - dependencies, build-gradle 등등..

---
+++ HealingPets/src/main/java/com/rocket/healingpets/HealingPetsApplication.java
@@ -0,0 +1,13 @@
+package com.rocket.healingpets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealingPetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealingPetsApplication.class, args);
+    }
+
+}
