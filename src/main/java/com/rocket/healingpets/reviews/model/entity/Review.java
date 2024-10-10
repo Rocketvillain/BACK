4ec2c325c8bf9b39dd8b9d2cd64a7ecbb68f6aee
권은혜 <a01027947353@gmail.com>
2024-10-10 15:16:38 +0900

    리뷰 관련 수정

--- src/main/java/com/rocket/healingpets/reviews/model/entity/Review.java
+++ src/main/java/com/rocket/healingpets/reviews/model/entity/Review.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 
@@ -15,6 +16,7 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @Builder(toBuilder = true)
+@EntityListeners(AuditingEntityListener.class)
 public class Review {
 
     @Id
